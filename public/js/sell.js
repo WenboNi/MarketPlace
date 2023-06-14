f3d9fcb1836fe5e4a4e5417292cdf7036fe13fa4
kimmh891223 <kimmh891223@hotmail.com>
2023-06-13 21:35:54 -0400

    changed sell.js into a POST req

--- public/js/sell.js
+++ public/js/sell.js
@@ -1,45 +1,43 @@
-const sellImageEl = document.getElementById('sellImage');
-const nameEl = document.getElementById('name');
-const categoryEl = document.getElementById('category');
-const priceEl = document.getElementById('price');
-const conditionEl = document.getElementById('condition');
-const descriptionEl = document.getElementById('description');
-const locationEl = document.getElementById('location');
-const contactEl = document.getElementById('contact');
-const submitButtonEl = document.getElementById('submitButton');
-
-
-
-submitButtonEl.addEventListener('click', function(event) {
+async function newFormHandler(event) {
     event.preventDefault();
     
-    const sellItemObject = {
-        product_name: nameEl.value,
-        category: categoryEl.value,
-        price: priceEl.value,
-        condition: conditionEl.value,
-        description: descriptionEl.value,
-        city: locationEl.value,
-        contact_info: contactEl.value
-    };
-
-    const file = sellImageEl.files[0];
-
+    const product_name = document.getElementById('name').value;
+    const category = document.getElementById('category').value;
+    const price = document.getElementById('price').value;
+    const condition = document.getElementById('condition').value;
+    const description = document.getElementById('description').value;
+    const location = document.getElementById('location').value;
+    const contact_info = document.getElementById('contact').value;
+    
+    const sellImageEl = document.getElementById('sellImage');
+    const uploadingFile = sellImageEl.files[0];
     const reader = new FileReader();
-
     reader.onload = function(e) {
-
         sellItemObject.image = e.target.result;
-
-        console.log(sellItemObject);
-
-        const sellItemJSON = JSON.stringify(sellItemObject);
-
-        //use sellItemJSON when doing POST function
-        console.log(sellItemJSON);
-
     };
-    reader.readAsDataURL(file);
-});
+    reader.readAsDataURL(uploadingFile);
 
-module.exports = { sellItemJSON, sellItemObject }
+    const response = await fetch(`/api/sell`, {
+      method: 'POST',
+      body: JSON.stringify({
+        product_name,
+        description,
+        category,
+        price,
+        condition,
+        location,
+        contact_info,
+        image
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to add the item');
+    }
+  }
+  
+  document.getElementById('submitButton').addEventListener('click', newFormHandler);
